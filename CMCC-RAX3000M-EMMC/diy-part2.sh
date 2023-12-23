7634d924bf50039031ef55d14c2d68d697259855
XXG <lalasou@qq.com>
2023-12-23 10:41:58 +0800

    Update diy-part2.sh

--- CMCC-RAX3000M-EMMC/diy-part2.sh
+++ CMCC-RAX3000M-EMMC/diy-part2.sh
@@ -10,14 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-##-----------------Add OpenClash dev core------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-##-----------------Delete DDNS's examples-----------------
-sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate   # 定制默认IP
+sed -i 's/ImmortalWrt/TPlink/g' package/base-files/files/bin/config_generate   # 定制默认主机名字
+
+# 移除重复软件包
+#rm -rf feeds/luci/themes/luci-theme-argon
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
