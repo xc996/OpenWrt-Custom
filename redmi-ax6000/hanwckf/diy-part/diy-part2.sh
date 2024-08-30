eb3f8f2a6ba23b60694bbd2f00bf16e43069c660
SuperArilo <superarilowh@gmail.com>
2024-08-30 11:16:52 +0800

    注释选项

--- redmi-ax6000/hanwckf/diy-part/diy-part2.sh
+++ redmi-ax6000/hanwckf/diy-part/diy-part2.sh
@@ -11,8 +11,8 @@
 #
 
 sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate   # 定制默认IP
-sed -i 's/ImmortalWrt/Bocchi/g' package/base-files/files/bin/config_generate   # 定制默认主机名字
-sed -i 's/  luci-app-quickstart / /g' target/linux/mediatek/Makefile  #移除默认quickstart
+# sed -i 's/ImmortalWrt/Bocchi/g' package/base-files/files/bin/config_generate   # 定制默认主机名字
+# sed -i 's/  luci-app-quickstart / /g' target/linux/mediatek/Makefile  #移除默认quickstart
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
