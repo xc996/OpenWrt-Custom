44c54562079a22cd77dd3b5234700915063a343e
SuperArilo <superarilowh@gmail.com>
2024-09-23 16:19:12 +0800

    update

--- redmi-ax6000/hanwckf/diy/diy-part1.sh
+++ redmi-ax6000/hanwckf/diy/diy-part1.sh
@@ -16,6 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#git clone https://github.com/messense/aliyundrive-webdav package/messense
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
-git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
--- redmi-ax6000/hanwckf/diy/diy-part2.sh
+++ redmi-ax6000/hanwckf/diy/diy-part2.sh
@@ -12,13 +12,3 @@
 
 sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-## openclash
-curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/raw/refs/heads/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-## openclash end
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
