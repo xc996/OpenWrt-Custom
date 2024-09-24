ca65d12c9b05522fae8f13d23f120164d7a1dc7d
SuperArilo <superarilowh@gmail.com>
2024-09-24 18:02:47 +0800

    try new update

--- redmi-ax6000/hanwckf/diy/diy-part1.sh
+++ redmi-ax6000/hanwckf/diy/diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+## 修改DTS的ubi为490MB的0x1ea00000
+sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000.dts
