7cd1e509e9d42ed3a53d1d45961070abec8f6fb1
SuperArilo <superarilowh@gmail.com>
2024-09-24 23:18:55 +0800

    try again

--- redmi-ax6000/padavanonly/diy/diy-part1.sh
+++ redmi-ax6000/padavanonly/diy/diy-part1.sh
@@ -15,6 +15,5 @@
 
 # Add a feed source
 ## 修改DTS的ubi为490MB的0x1ea00000
-sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
-## 修改DTS的spi_nand的spi-max-frequency为52MHz，52000000
-sed -i 's/spi-max-frequency = <20000000>/spi-max-frequency = <52000000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
+sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000.dts
+#
