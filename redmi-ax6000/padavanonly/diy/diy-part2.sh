954d46061952f6698fc7cdbf9177b4a918df299e
SuperArilo <superarilowh@gmail.com>
2024-09-24 17:59:09 +0800

    restore

--- redmi-ax6000/padavanonly/diy/diy-part2.sh
+++ redmi-ax6000/padavanonly/diy/diy-part2.sh
@@ -9,12 +9,5 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
 sed -i 's/192.168.6.1/172.16.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-
-##-----------------Delete DDNS's examples-----------------
-sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
