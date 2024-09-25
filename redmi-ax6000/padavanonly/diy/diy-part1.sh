f5196787a3e7db77fc4615a4d930b5b80ea5869f
SuperArilo <superarilowh@gmail.com>
2024-09-25 14:15:34 +0800

    修正

--- redmi-ax6000/padavanonly/diy/diy-part1.sh
+++ redmi-ax6000/padavanonly/diy/diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 ## 修改DTS的ubi为490MB的0x1ea00000
 sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000.dts
-#
