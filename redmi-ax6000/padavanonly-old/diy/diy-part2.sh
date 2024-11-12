10280887533a00b03bdd88fc1f4f95aee10f5396
SuperArilo <superarilowh@gmail.com>
2024-11-12 15:11:36 +0800

    更新 old

--- redmi-ax6000/padavanonly-old/diy/diy-part2.sh
+++ redmi-ax6000/padavanonly-old/diy/diy-part2.sh
@@ -13,3 +13,5 @@ sed -i 's/192.168.6.1/172.16.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 sed -i 's/luci-app-wrtbwmon//g' target/linux/mediatek/Makefile
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
