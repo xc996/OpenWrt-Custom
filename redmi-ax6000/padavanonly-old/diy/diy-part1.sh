eedd6a7a7c35c19eaa71e8d988daada4c757b279
SuperArilo <superarilowh@gmail.com>
2025-01-20 22:11:18 +0800

    add wol

--- redmi-ax6000/padavanonly-old/diy/diy-part1.sh
+++ redmi-ax6000/padavanonly-old/diy/diy-part1.sh
@@ -20,3 +20,4 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone https://github.com/animegasan/luci-app-wolplus.git package/luci-app-wolplus
