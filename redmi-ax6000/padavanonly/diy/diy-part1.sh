14515a20a98f4a484eb6f5f3b9f8574f0372cb80
SuperArilo <superarilowh@gmail.com>
2025-02-23 21:00:54 +0800

    remove oaf

--- redmi-ax6000/padavanonly/diy/diy-part1.sh
+++ redmi-ax6000/padavanonly/diy/diy-part1.sh
@@ -21,5 +21,4 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone https://github.com/SuperArilo/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/Firsgith/luci-app-wolplus.git package/luci-app-wolplus
-git clone https://github.com/Lienol/openwrt-OpenAppFilter.git package/open-app-filter
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
