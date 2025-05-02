c15a167a78d1d2eb2a8fc9d6f1f91b6d83b7224c
xc996 <becomebr79@gmail.com>
2025-05-02 21:57:44 +0800

    专注

--- redmi-ax6000/padavanonly-24.10-110/diy/diy-part2.sh
+++ redmi-ax6000/padavanonly-24.10-110/diy/diy-part2.sh
@@ -9,7 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-sed -i 's/192.168.6.1/172.16.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 sed -i 's/luci-app-wrtbwmon//g' target/linux/mediatek/Makefile
